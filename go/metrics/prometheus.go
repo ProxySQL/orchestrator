63debf266eaac106d7632bbc7e16eef146fa173f
Robin F. Pronk <robin.pronk@nedap.com>
2026-05-27 12:40:29 +0200

    Add basic health metrics to prometheus exporter
    Signed-off-by: Robin F. Pronk <robin.pronk@nedap.com>

--- go/metrics/prometheus.go
+++ go/metrics/prometheus.go
@@ -17,9 +17,13 @@
 package metrics
 
 import (
+	"sync/atomic"
+
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/proxysql/golib/log"
 	"github.com/proxysql/orchestrator/go/config"
+	"github.com/proxysql/orchestrator/go/process"
+	orcraft "github.com/proxysql/orchestrator/go/raft"
 )
 
 // Prometheus metric variables, exported so other packages can update them.
@@ -54,6 +58,43 @@ var (
 		Help:    "Duration of recovery operations in seconds.",
 		Buckets: prometheus.ExponentialBuckets(0.5, 2, 12), // 0.5s to ~1024s
 	})
+
+	HealthIsReady = prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+		Name: "orchestrator_health_is_ready",
+		Help: "1 if the node is ready, 0 otherwise.",
+	}, func() float64 {
+		if atomic.LoadInt64(&process.LastContinousCheckHealthy) == 1 {
+			return 1
+		}
+		return 0
+	})
+
+	HealthIsLive = prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+		Name: "orchestrator_health_is_live",
+		Help: "1 if the node is live, 0 otherwise.",
+	}, func() float64 {
+		return 1
+	})
+
+	RaftIsHealthy = prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+		Name: "orchestrator_raft_is_healthy",
+		Help: "1 if the node is a healthy raft node, 0 otherwise.",
+	}, func() float64 {
+		if orcraft.IsRaftEnabled() && orcraft.IsHealthy() {
+			return 1
+		}
+		return 0
+	})
+
+	RaftIsLeader = prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+		Name: "orchestrator_raft_is_leader",
+		Help: "1 if the node is the raft leader, 0 otherwise.",
+	}, func() float64 {
+		if orcraft.IsRaftEnabled() && orcraft.IsLeader() {
+			return 1
+		}
+		return 0
+	})
 )
 
 // InitPrometheus registers all Prometheus metrics. Should be called once at
@@ -70,5 +111,9 @@ func InitPrometheus() {
 		ClustersTotal,
 		RecoveriesTotal,
 		RecoveryDurationSeconds,
+		HealthIsReady,
+		HealthIsLive,
+		RaftIsHealthy,
+		RaftIsLeader,
 	)
 }
